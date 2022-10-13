35e8f97bd25ece6f663058a2bfe71c797e1af223
Yuki Inamoto <rasenganball122@gmail.com>
2022-10-13 15:28:56 +0000

    CRUD Tasks

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,19 @@
+@extends("layouts.app")
+
+@section("content")
+    <h1>タスク{{$task->id}}の編集ページ</h1>
+    
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ["route"=>["tasks.update", $task->id], "method"=>"put"]) !!}
+                <div class="form-group">
+                    {!! Form::label("content", "タスク内容") !!}
+                    {!! Form::text("content", null, ["class"=>"form-control"])!!}
+                </div>
+                
+                {!! Form::submit("更新", ["class"=>"btn btn-light"]) !!}
+            
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
