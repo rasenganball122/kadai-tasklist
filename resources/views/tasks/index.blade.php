7a472a871a870b22fe3fb474e5ecf87002d1ee18
Yuki Inamoto <rasenganball122@gmail.com>
2022-11-02 10:53:03 +0000

    user registration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,14 @@
 @extends("layouts.app")
 
 @section("content")
+    <div class="center jumbotron">
+        <<div class="text-center">
+            <h1>Welcome to Tasklist!</h1>
+             {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route("signup.get", "Sign up now!", [], ["class"=>"btn btn-lg btn-primary"]) !!}
+        </div>
+    </div>
+   
     <h1>タスク一覧</h1>
     
     @if(count($tasks)>0)
